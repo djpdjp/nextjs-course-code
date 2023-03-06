e6f3e9b9cd5cc383fdcaad37e45bdd2879da08d5
David Payne <djpdjp16@gmail.com>
2023-03-05 22:00:07 -0600

    5.102

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
+import fs from 'fs/promises';
+import path from 'path';
+import Link from "next/link";
+
 function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+    const { products } = props;
+
+    return (
+        <ul>
+            {products.map(p => <li key={p.id}><Link href={`/${p.id}`}>{p.title}</Link></li>)}
+        </ul>
+    );
+}
+
+
+
+export async function getStaticProps(context) {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    if(data.products.length === 0){
+        return {notFound: true}
+    }
+
+    return {
+        props: {
+            products: data.products
+        },
+        revalidate: 120,
+
+    };
 }
 
 export default HomePage;
+
